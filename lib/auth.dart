dfdd996560e27e863153766c93e34107baeb902c
Fred Herbert <fredittoherman@gmail.com>
2023-03-14 18:39:08 +0300

    commit

---
+++ lib/auth.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+// **************** Login starts here ***************************
+class LoginScreen extends StatefulWidget {
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
+// **************** Login Ends here ***************************
+
+
+
+
+// **************** Register starts here ***************************
+class RegisterScreen extends StatefulWidget {
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
+// **************** Register Ends here ***************************
